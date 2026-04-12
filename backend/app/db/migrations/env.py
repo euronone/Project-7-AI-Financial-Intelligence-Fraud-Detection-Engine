36d22f870b991f4129eb90c431276c5785c770f2
Sunil Gupta <sunil5807516@gmail.com>
2026-04-12 20:45:59 +0530

    Notification System Update

--- backend/app/db/migrations/env.py
+++ backend/app/db/migrations/env.py
@@ -4,7 +4,7 @@ import os
 from logging.config import fileConfig
 
 from sqlalchemy import pool
-from sqlalchemy.ext.asyncio import async_engine_from_config, create_async_engine
+from sqlalchemy.ext.asyncio import create_async_engine
 from alembic import context
 
 # Load all models so Alembic can auto-detect schema
